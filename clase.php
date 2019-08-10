abbfc0bce3018126d52d674490823e3f3f8190af
critofer_asosa <cristoferasosa@gmail.com>
2019-08-09 20:49:04 -0600

    Carga Project

---
+++ clase.php
@@ -0,0 +1,17 @@
+<?php
+echo $nombre=$_POST['nombre'],"<br>";
+echo $apellido=$_POST['apellido'],"<br>";
+echo $genero=$_POST['genero'],"<br>";
+echo $edad=$_POST['edad'],"<br>";
+echo $telefono=$_POST['telefono'],"<br>";
+echo $email=$_POST['email'],"<br>";
+
+
+echo $vehiculo=$_POST['tipocarro'],"<br>";
+echo $marca=$_POST['marcarro'],"<br>";
+echo $modelo=$_POST['model'],"<br>";
+echo $anio=$_POST['año'],"<br>";
+echo $color=$_POST['colore'],"<br>";
+
+
+?>
